13ce9bccf116c827144c579ae7a383233247e3dd
Chanison Aupathin <chanisonaupathin@gmail.com>
2025-06-26 16:36:50 +0700

    Chanison

---
+++ List/main.py
@@ -0,0 +1,16 @@
+# append() object to the end of the list 
+# extend() add many list to the end of the list
+# insert() insert object before index
+#remove() remove frist element
+#clear() 
+# sort()
+# reverse()
+colors = ['red','blue','orange','black','white','yellow',"red"]
+# colors.append("brown")
+colors.extend(['brown' , 'pink'])
+# colors.insert(1 , "gary")
+# colors.remove("blue")
+# colors.clear()
+# colors.sort()
+colors.reverse()
+print(colors)
